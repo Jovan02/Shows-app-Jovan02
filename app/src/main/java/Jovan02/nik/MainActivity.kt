474230474651e0620943b86b311417b3eff3504a
Jovan02 <46245320+Jovan02@users.noreply.github.com>
2022-07-07 16:38:34 +0200

    Project first commit

---
+++ app/src/main/java/Jovan02/nik/MainActivity.kt
@@ -0,0 +1,11 @@
+package Jovan02.nik
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
