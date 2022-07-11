943bebadcf69cecf627a23a184830b6754cc20bc
Jovan02 <46245320+Jovan02@users.noreply.github.com>
2022-07-11 19:44:18 +0200

    Make floating label Email functional

--- app/src/main/java/com/jovannikolic/myapplication/LoginActivity.kt
+++ app/src/main/java/com/jovannikolic/myapplication/LoginActivity.kt
@@ -2,10 +2,33 @@ package com.jovannikolic.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.jovannikolic.myapplication.databinding.ActivityLoginBinding
 
 class ActivityLogin : AppCompatActivity() {
+
+    lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+
+        setContentView(binding.root)
+
+        binding.emailtext.setOnFocusChangeListener { _, focused ->
+            if(!binding.emailtext.text.toString().equals("") || focused){
+                binding.emaillabel.setVisibility(View.VISIBLE)
+                binding.emailtext.hint = " "
+            }else if(binding.emailtext.text.toString().equals("") || focused){
+                binding.emaillabel.setVisibility(View.INVISIBLE)
+                binding.emailtext.hint = "Email"
+            }
+        }
+
+
+
+
     }
 }
