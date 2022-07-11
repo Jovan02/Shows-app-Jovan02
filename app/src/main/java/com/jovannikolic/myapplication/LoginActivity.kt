a1d9f7cdda7c09e7cc88dab6d2e322718fb4890c
Jovan02 <46245320+Jovan02@users.noreply.github.com>
2022-07-11 19:53:28 +0200

    Make show/hide button functional Make floating label Password functional Add necessary .xml files such as invisible_button.xml and visible_button.xml

--- app/src/main/java/com/jovannikolic/myapplication/LoginActivity.kt
+++ app/src/main/java/com/jovannikolic/myapplication/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.jovannikolic.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.view.View
 import com.jovannikolic.myapplication.databinding.ActivityLoginBinding
 
@@ -17,6 +19,9 @@ class ActivityLogin : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        var pressed = true
+
+        //  Floating Email label
         binding.emailtext.setOnFocusChangeListener { _, focused ->
             if(!binding.emailtext.text.toString().equals("") || focused){
                 binding.emaillabel.setVisibility(View.VISIBLE)
@@ -26,6 +31,36 @@ class ActivityLogin : AppCompatActivity() {
                 binding.emailtext.hint = "Email"
             }
         }
+
+        //  Show/Hide button
+        binding.visiblebutton.setOnClickListener {
+            if (pressed){
+                binding.visiblebutton.setBackgroundResource(R.drawable.invisible_button)
+                binding.passwordtext.transformationMethod = PasswordTransformationMethod.getInstance()
+                pressed = false
+            }else{
+                binding.visiblebutton.setBackgroundResource(R.drawable.visible_button)
+                binding.passwordtext.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                pressed = true
+            }
+        }
+
+        //  Floating Password label and Show/Hide button
+        binding.passwordtext.setOnFocusChangeListener { _, focused ->
+
+            if(!binding.passwordtext.text.toString().equals("") || focused){
+                binding.visiblebutton.setVisibility(View.VISIBLE)
+                binding.passwordlabel.setVisibility(View.VISIBLE)
+                binding.passwordtext.hint = " "
+            }else if(binding.passwordtext.text.toString().equals("") || focused){
+                binding.visiblebutton.setVisibility(View.GONE)
+                binding.passwordlabel.setVisibility(View.INVISIBLE)
+                binding.passwordtext.hint = "Password"
+            }
+        }
+
+
+
 
 
 
