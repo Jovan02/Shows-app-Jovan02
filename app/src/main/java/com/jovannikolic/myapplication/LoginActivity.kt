3a3bf05035c648dc24dfbcfc9cc66969bb9b5784
Jovan02 <46245320+Jovan02@users.noreply.github.com>
2022-07-11 20:02:10 +0200

    Add login_button.xml Make Login Button functional Make validEmail function Make validPassword function

--- app/src/main/java/com/jovannikolic/myapplication/LoginActivity.kt
+++ app/src/main/java/com/jovannikolic/myapplication/LoginActivity.kt
@@ -1,9 +1,13 @@
 package com.jovannikolic.myapplication
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
+import android.util.Patterns
 import android.view.View
 import com.jovannikolic.myapplication.databinding.ActivityLoginBinding
 
@@ -60,10 +64,73 @@ class ActivityLogin : AppCompatActivity() {
         }
 
 
+        //  Login button - active if required conditions are fulfilled
+        val generalTextWatcher = object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
 
+            }
 
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if(binding.passwordtext.text.toString().length > 6 && binding.emailtext.text.toString().isNotEmpty() && validEmail().equals("")){
+                    binding.loginbutton.isEnabled = true
+                    binding.loginbutton.isClickable = true
+                    binding.loginbutton.setBackgroundResource(R.drawable.login_button)
+                }else{
+                    binding.loginbutton.isEnabled = false
+                    binding.loginbutton.isClickable = false
+                    binding.loginbutton.setBackgroundResource(R.drawable.login_button_not_clickable)
+                }
+
+                if(!validEmail().equals("")){
+                    //binding.emailtext.setError(validEmail())
+                    binding.emailerror.text = validEmail()
+                }else{
+                    //binding.emailtext.setError(null)
+                    binding.emailerror.text = null
+                }
+
+                if(!validPassword().equals("")){
+                    //binding.passwordtext.setError(validPassword())
+                    binding.passworderror.text = validPassword()
+                }else{
+                    //binding.passwordtext.setError(null)
+                    binding.passworderror.text = null
+                }
+
+            }
+        }
+        binding.emailtext.addTextChangedListener(generalTextWatcher)
+        binding.passwordtext.addTextChangedListener(generalTextWatcher)
 
 
 
     }
+
+    private fun validEmail(): String {
+
+        val emailText = binding.emailtext.text.toString()
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()){
+            return "Invalid Email Address"
+        }
+        return ""
+    }
+
+    private fun validPassword(): String {
+
+        val passText = binding.passwordtext.text.toString()
+
+        if(passText.length < 7) {
+            return "Password must be at least 7 characters long."
+        }
+
+        return ""
+    }
+
+
+
 }
