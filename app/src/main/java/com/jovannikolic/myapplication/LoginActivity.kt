44e398466e6375f52e12646a0c95dec77787ba6a
Jovan02 <46245320+Jovan02@users.noreply.github.com>
2022-07-11 20:10:56 +0200

    Fix themes.xml for button Remove excessive library from LoginActivity.kt

--- app/src/main/java/com/jovannikolic/myapplication/LoginActivity.kt
+++ app/src/main/java/com/jovannikolic/myapplication/LoginActivity.kt
@@ -1,6 +1,5 @@
 package com.jovannikolic.myapplication
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
