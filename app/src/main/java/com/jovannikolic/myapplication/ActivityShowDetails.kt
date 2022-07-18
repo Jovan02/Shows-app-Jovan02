83ff0b427c9aaa73d452b05b0d36c971a51867ff
Jovan02 <46245320+Jovan02@users.noreply.github.com>
2022-07-18 18:22:44 +0200

    Edit view_show_item.xmlEdit activity_show_details.xml Change description of shows in ActivityShows.kt Add bottom sheet and connect ActivityShowDetails.kt with ActivityShows.kt

--- app/src/main/java/com/jovannikolic/myapplication/ActivityShowDetails.kt
+++ app/src/main/java/com/jovannikolic/myapplication/ActivityShowDetails.kt
@@ -2,7 +2,9 @@ package com.jovannikolic.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.jovannikolic.myapplication.databinding.ActivityShowDetailsBinding
+import com.jovannikolic.myapplication.databinding.DialogAddReviewBinding
 
 class ActivityShowDetails : AppCompatActivity() {
 
@@ -15,12 +17,35 @@ class ActivityShowDetails : AppCompatActivity() {
         binding = ActivityShowDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        getData()
 
-
+        clickReviewButton()
 
 
 
 
 
     }
+
+    private fun showBottomSheet(){
+        val dialog = BottomSheetDialog(this)
+
+        val bottomSheetBinding = DialogAddReviewBinding.inflate(layoutInflater)
+        dialog.setContentView(bottomSheetBinding.root)
+
+        dialog.show()
+    }
+
+    private fun clickReviewButton(){
+        binding.reviewbutton.setOnClickListener{
+            showBottomSheet()
+        }
+    }
+
+    private fun getData(){
+        binding.maintitle.text = intent.extras?.getString("title")
+        binding.showimg.setImageResource(intent.getIntExtra("imageResource", 0))
+        binding.showtext.text = intent.extras?.getString("description")
+    }
+
 }
--- app/src/main/java/com/jovannikolic/myapplication/ActivityShows.kt
+++ app/src/main/java/com/jovannikolic/myapplication/ActivityShows.kt
@@ -21,11 +21,11 @@ class ActivityShows : AppCompatActivity() {
     }
 
     private val shows = listOf(
-        Show(0, "Family Guy", "Family Guy is an American adult animated sitcom created by Seth...", R.drawable.family_guy),
-        Show(1, "Simpsons", "The Simpsons is an American animated sitcom created by Matt Groening. The...", R.drawable.simpsons),
-        Show(2, "Prison Break", "Prison Break is an American serial drama television series created by Paul...", R.drawable.prison_break),
-        Show(3, "Sherlock", "Sherlock is a British mystery crime drama television series based on Sir Arthur...", R.drawable.sherlock),
-        Show(4, "Witcher", "The Witcher is a Polish-American fantasy drama television series created by...", R.drawable.witcher),
+        Show(0, "Family Guy", "Family Guy is an American adult animated sitcom created by Seth MacFarlane. The series centers on the Griffins, a family consisting of parents Peter and Lois; their children, Meg, Chris, and Stewie; and their anthropomorphic pet dog, Brian. Set in the fictional city of Quahog, Rhode Island, the show exhibits much of its humor in the form of metafictional cutaway gags that often lampoon American culture.", R.drawable.family_guy),
+        Show(1, "Simpsons", "The Simpsons is an American animated sitcom created by Matt Groening. The series is a satirical depiction of American life, epitomized by the Simpson family, which consists of Homer, Marge, Bart, Lisa, and Maggie. The show is set in the fictional town of Springfield and parodies American culture and society, television, and the human condition.", R.drawable.simpsons),
+        Show(2, "Prison Break", "Prison Break is an American serial drama television series created by Paul Scheuring. The series revolves around two brothers, Lincoln Burrows and Michael Scofield. Burrows has been sentenced to death for a crime he did not commit, and Scofield devises an elaborate plan to help his brother escape prison and clear his name.", R.drawable.prison_break),
+        Show(3, "Sherlock", "Sherlock is a British mystery crime drama television series based on Sir Arthur Conan Doyle's Sherlock Holmes detective stories. Sherlock depicts \"consulting detective\" Sherlock Holmes solving various mysteries in modern-day London. Holmes is assisted by his flatmate and friend, Dr John Watson , who has returned from military service in Afghanistan with the Royal Army Medical Corps.", R.drawable.sherlock),
+        Show(4, "Witcher", "The Witcher is a Polish-American fantasy drama television series created by Lauren Schmidt Hissrich, based on the book series of the same name by Polish writer Andrzej Sapkowski. Set on a fictional, medieval-inspired landmass known as \"the Continent\", The Witcher explores the legend of Geralt of Rivia and Princess Ciri, who are linked to each other by destiny.", R.drawable.witcher),
         Show(5, "Friends", "Friends is an American television sitcom created by David Crane and Marta...", R.drawable.friends),
     )
 
@@ -57,7 +57,13 @@ class ActivityShows : AppCompatActivity() {
     private fun initShowsRecycler(){
 
         adapter = ShowsAdapter(shows){ show ->
-            Toast.makeText(this, show.name, Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this, show.name, Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, ActivityShowDetails:: class.java)
+            intent.putExtra("title", show.name)
+            intent.putExtra("imageResource", show.imageResourceId)
+            intent.putExtra("description", show.description)
+
+            startActivity(intent)
         }
 
         binding.showsrecycler.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
