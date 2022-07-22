1490ecddc97bf462dce878448633cb0f35157773
Jovan02 <46245320+Jovan02@users.noreply.github.com>
2022-07-22 13:30:03 +0200

    Add Parcelable to Show.kt

--- app/src/main/java/models/Show.kt
+++ app/src/main/java/models/Show.kt
@@ -1,11 +1,43 @@
 package models
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.annotation.DrawableRes
+
 
 data class Show (
     val ID: Int,
     val name: String,
     val description: String,
     @DrawableRes val imageResourceId: Int
-)
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readInt()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(ID)
+        parcel.writeString(name)
+        parcel.writeString(description)
+        parcel.writeInt(imageResourceId)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Show> {
+        override fun createFromParcel(parcel: Parcel): Show {
+            return Show(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Show?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
 
