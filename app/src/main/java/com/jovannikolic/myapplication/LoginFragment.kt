3cd01474258384e4bdc3d0275bb1cebb1f1d267a
Jovan02 <46245320+Jovan02@users.noreply.github.com>
2022-07-24 20:57:55 +0200

    Override back button it closes app

--- app/src/main/java/com/jovannikolic/myapplication/LoginFragment.kt
+++ app/src/main/java/com/jovannikolic/myapplication/LoginFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.core.content.edit
 import androidx.core.widget.addTextChangedListener
 import androidx.navigation.fragment.findNavController
@@ -36,6 +37,13 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initListeners()
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                System.exit(0)
+            }
+        })
+
     }
 
     private fun initListeners() {
