12ccfe8fe595383d538b029eb4d0ced8d4db3b9c
Jovan02 <46245320+Jovan02@users.noreply.github.com>
2022-07-28 11:09:58 +0200

    Move User to separate class

--- app/src/main/java/models/RegisterResponse.kt
+++ app/src/main/java/models/RegisterResponse.kt
@@ -7,10 +7,3 @@ import kotlinx.serialization.Serializable
 data class RegisterResponse(
     @SerialName("user") val user: User
 )
-
-@Serializable
-data class User(
-    @SerialName("id") val id: String,
-    @SerialName("email") val email: String,
-    @SerialName("image_url") val imageUrl: String?
-)
