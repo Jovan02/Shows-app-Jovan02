adc7416794780cae117fc6839b090d0ba14e4bca
Jovan02 <46245320+Jovan02@users.noreply.github.com>
2022-07-28 11:11:31 +0200

    Create updatePhoto function Create UpdatePhotoRequest.kt Create UpdatePhotoResponse.kt

--- app/src/main/java/networking/ShowsApiService.kt
+++ app/src/main/java/networking/ShowsApiService.kt
@@ -4,9 +4,12 @@ import models.LoginRequest
 import models.LoginResponse
 import models.RegisterRequest
 import models.RegisterResponse
+import models.UpdatePhotoRequest
+import models.UpdatePhotoResponse
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
+import retrofit2.http.PUT
 
 interface ShowsApiService {
 
@@ -15,4 +18,8 @@ interface ShowsApiService {
 
     @POST("/users/sign_in")
     fun login(@Body request: LoginRequest): Call<LoginResponse>
+
+    @PUT("/users")
+    fun updatePhoto(@Body request: UpdatePhotoRequest): Call<UpdatePhotoResponse>
+
 }
