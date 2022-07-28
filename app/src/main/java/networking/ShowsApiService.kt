ce69309c636e90ff16759f29e5522781591405da
Jovan02 <46245320+Jovan02@users.noreply.github.com>
2022-07-28 14:57:18 +0200

    Change to MultipartBody.Part

--- app/src/main/java/networking/ShowsApiService.kt
+++ app/src/main/java/networking/ShowsApiService.kt
@@ -6,8 +6,11 @@ import models.RegisterRequest
 import models.RegisterResponse
 import models.UpdatePhotoRequest
 import models.UpdatePhotoResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.PUT
 
@@ -20,6 +23,7 @@ interface ShowsApiService {
     fun login(@Body request: LoginRequest): Call<LoginResponse>
 
     @PUT("/users")
-    fun updatePhoto(@Body request: UpdatePhotoRequest): Call<UpdatePhotoResponse>
+    fun updatePhoto(@Body request: MultipartBody.Part, create: RequestBody): Call<UpdatePhotoResponse>
+
 
 }
