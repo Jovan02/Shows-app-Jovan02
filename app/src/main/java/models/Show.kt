3e280cc1738bdbd7fcf82bf1f15cd3ff6aa86850
Jovan02 <46245320+Jovan02@users.noreply.github.com>
2022-07-30 01:47:26 +0200

    Change average_rating to Float

--- app/src/main/java/models/Show.kt
+++ app/src/main/java/models/Show.kt
@@ -8,7 +8,7 @@ import kotlinx.serialization.Serializable
 @Parcelize
 data class Show(
     val id: String,
-    val average_rating: Int,
+    val average_rating: Float,
     val description: String,
     val image_url: String,
     val no_of_reviews: Int = 0,
