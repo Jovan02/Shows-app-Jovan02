787f01ab48abdff631ddbebe477e3885258a7e4b
Jovan02 <46245320+Jovan02@users.noreply.github.com>
2022-08-02 14:52:40 +0200

    Move all logic to RegisterViewModel.kt Move text to strings.xml

--- app/src/main/java/com/jovannikolic/myapplication/RegisterFragment.kt
+++ app/src/main/java/com/jovannikolic/myapplication/RegisterFragment.kt
@@ -47,94 +47,12 @@ class RegisterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initListeners()
-    }
-
-    @SuppressLint("SetTextI18n")
-    private fun initListeners() {
-        var isEmailValid = false
-        var isPasswordValid = false
-        var isRepeatPasswordValid = false
-
-        //  Email TextWatcher
-        binding.emailtext.editText?.addTextChangedListener {
-            if (binding.emailtext.editText?.text.toString().isNotEmpty() && validEmail()) {
-                isEmailValid = true
-                binding.emailerror.text = null
-            } else {
-                isEmailValid = false
-                binding.emailerror.text = "Invalid Email Address"
-            }
-            checkRegisterButtonState(isEmailValid, isPasswordValid, isRepeatPasswordValid)
-        }
-
-        //  Password TextWatcher
-        binding.passwordtext.editText?.addTextChangedListener {
-            if (validPassword()) {
-                isPasswordValid = true
-                binding.passworderror.text = null
-            } else {
-                isPasswordValid = false
-                binding.passworderror.text = "Password must be at least 6 characters long."
-            }
-            checkRegisterButtonState(isEmailValid, isPasswordValid, isRepeatPasswordValid)
-        }
-
-        //  Repeat Password TextWatcher
-        binding.repeatPasswordText.editText?.addTextChangedListener {
-            if (validRepeatPassword()) {
-                isRepeatPasswordValid = true
-                binding.passworderror.text = null
-            } else {
-                isRepeatPasswordValid = false
-                binding.passworderror.text = "Passwords do not match."
-            }
-            checkRegisterButtonState(isEmailValid, isPasswordValid, isRepeatPasswordValid)
-        }
-
-        binding.registerButton.setOnClickListener {
-            viewModel.sendDataToApi(
-                requireContext(),
-                this,
-                binding.emailtext.editText?.text.toString(),
-                binding.passwordtext.editText?.text.toString(),
-                binding.repeatPasswordText.editText?.text.toString()
-            )
-        }
-
+        viewModel.initListeners(requireContext(), this, binding)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    private fun validEmail(): Boolean {
-        val emailText = binding.emailtext.editText?.text.toString()
-
-        if (!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()) {
-            return false
-        }
-        return true
-    }
-
-    private fun validPassword(): Boolean {
-        val passText = binding.passwordtext.editText?.text.toString()
-
-        if (passText.length < 6) {
-            return false
-        }
-        return true
-    }
-
-    private fun validRepeatPassword(): Boolean =
-        binding.passwordtext.editText?.text.toString() == binding.repeatPasswordText.editText?.text.toString()
-
-    private fun checkRegisterButtonState(email: Boolean, password: Boolean, repeatPassword: Boolean) {
-        binding.registerButton.isEnabled = email && password && repeatPassword
-        if (email && password && repeatPassword) {
-            binding.registerButton.setTextColor(Color.parseColor("#52369C"))
-        }
     }
 
 }
