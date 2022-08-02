16e301cc16c6632b0a0c71c559a09134dc0932f0
Jovan02 <46245320+Jovan02@users.noreply.github.com>
2022-08-03 00:37:54 +0200

    Add Review table to the database

--- app/src/main/java/database/ShowsDatabase.kt
+++ app/src/main/java/database/ShowsDatabase.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import models.Review
 import models.Show
 
 @Database(
     entities = [
-        Show::class
+        Show::class,
+        Review::class
     ],
     version = 1
 )
@@ -35,4 +37,5 @@ abstract class ShowsDatabase : RoomDatabase(){
         }
     }
     abstract fun showDao(): ShowDao
+    abstract fun reviewDao(): ReviewDao
 }
