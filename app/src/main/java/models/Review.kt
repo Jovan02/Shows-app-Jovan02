74a6136c5d2d2923f02e4bfc2f83e938a16d1e57
Jovan02 <46245320+Jovan02@users.noreply.github.com>
2022-08-03 00:37:00 +0200

    Adjust Review.kt for database

--- app/src/main/java/models/Review.kt
+++ app/src/main/java/models/Review.kt
@@ -1,12 +1,16 @@
 package models
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.serialization.Serializable
 
 @Serializable
+@Entity(tableName = "review")
 data class Review(
-    var id: String,
-    var comment: String,
-    var rating: Int,
-    var show_id: Int,
-    var user: User
+    @PrimaryKey var id: String,
+    @ColumnInfo(name = "comment") var comment: String,
+    @ColumnInfo(name = "rating") var rating: Int,
+    @ColumnInfo(name = "show_id") var show_id: Int,
+    @ColumnInfo(name = "user") var user: User
 )
