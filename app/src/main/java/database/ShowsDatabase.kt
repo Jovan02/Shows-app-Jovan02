354306b4ed2b076052cb6482bc9535f423b4e611
Jovan02 <46245320+Jovan02@users.noreply.github.com>
2022-08-02 18:00:20 +0200

    Change to Show

--- app/src/main/java/database/ShowsDatabase.kt
+++ app/src/main/java/database/ShowsDatabase.kt
@@ -4,10 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import models.Show
 
 @Database(
     entities = [
-        ShowEntity::class
+        Show::class
     ],
     version = 1
 )
