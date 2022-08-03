b59c10f315bcd8da4d797c585896969c4ba34772
Jovan02 <46245320+Jovan02@users.noreply.github.com>
2022-08-03 12:59:04 +0200

    Insert Shows and Reviews first time

--- app/src/main/java/com/jovannikolic/myapplication/MainApplication.kt
+++ app/src/main/java/com/jovannikolic/myapplication/MainApplication.kt
@@ -3,6 +3,8 @@ package com.jovannikolic.myapplication
 import android.app.Application
 import database.ShowsDatabase
 import java.util.concurrent.Executors
+import models.Review
+import models.ReviewEntity
 import models.Show
 
 class MainApplication : Application() {
@@ -12,11 +14,15 @@ class MainApplication : Application() {
     }
 
     private val shows = emptyList<Show>()
+    private val reviews = emptyList<ReviewEntity>()
 
     override fun onCreate() {
         super.onCreate()
         Executors.newSingleThreadExecutor().execute{
             database.showDao().insertAllShows(shows)
+        }
+        Executors.newSingleThreadExecutor().execute{
+            database.reviewDao().insertAllReviews(reviews)
         }
     }
 }
