48a42ee882c2ecf48726d82edd13947e32653e44
Jovan02 <46245320+Jovan02@users.noreply.github.com>
2022-08-03 17:30:08 +0200

    Move logic to RegisterViewModel.kt

--- app/src/main/java/com/jovannikolic/myapplication/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/jovannikolic/myapplication/ui/register/RegisterFragment.kt
@@ -7,7 +7,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.jovannikolic.myapplication.R
 import com.jovannikolic.myapplication.databinding.FragmentRegisterBinding
 import networking.ApiModule
 
@@ -35,7 +39,55 @@ class RegisterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.initListeners(requireContext(), this, binding)
+        initListeners()
+        initObservers()
+    }
+
+    private fun initObservers(){
+        viewModel.emailHasErrorLiveData.observe(viewLifecycleOwner){ hasError ->
+            binding.emailerror.text = if(hasError) "Invalid Email Address" else null
+        }
+
+        viewModel.passwordHasErrorLiveData.observe(viewLifecycleOwner){ hasError ->
+            binding.passworderror.text = if (hasError) "Password must be at least 6 characters long." else null
+        }
+
+        viewModel.passwordRepeatHasErrorLiveData.observe(viewLifecycleOwner){ hasError ->
+            binding.passworderror.text = if (hasError) "Passwords don't match." else null
+        }
+
+        viewModel.buttonIsEnabledLiveData.observe(viewLifecycleOwner){ isEnabled ->
+            binding.registerButton.isEnabled = isEnabled
+            binding.registerButton.setTextColor(if (isEnabled) requireContext().getColor(R.color.purple_600) else requireContext().getColor(R.color.white))
+        }
+
+        viewModel.isRegistered.observe(viewLifecycleOwner){ isRegistered ->
+            if(isRegistered){
+                findNavController().navigate(RegisterFragmentDirections.toLoginFragment(true))
+            } else if(isRegistered){
+                Toast.makeText(context, R.string.registration_failed, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    private fun initListeners() {
+
+        binding.emailtext.editText?.addTextChangedListener {
+            viewModel.emailChanged(binding.emailtext.editText?.text.toString())
+        }
+
+        binding.passwordtext.editText?.addTextChangedListener {
+            viewModel.passwordChanged(binding.passwordtext.editText?.text.toString())
+        }
+
+        binding.repeatPasswordText.editText?.addTextChangedListener {
+            viewModel.passwordRepeatChanged(binding.repeatPasswordText.editText?.text.toString())
+        }
+
+        binding.registerButton.setOnClickListener {
+            viewModel.register()
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/jovannikolic/myapplication/ui/register/RegisterViewModel.kt
+++ app/src/main/java/com/jovannikolic/myapplication/ui/register/RegisterViewModel.kt
@@ -1,15 +1,9 @@
 package com.jovannikolic.myapplication.ui.register
 
-import android.content.Context
-import android.graphics.Color
 import android.util.Patterns
-import android.widget.Toast
-import androidx.core.widget.addTextChangedListener
-import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
-import com.jovannikolic.myapplication.R
-import com.jovannikolic.myapplication.databinding.FragmentRegisterBinding
 import models.RegisterRequest
 import models.RegisterResponse
 import networking.ApiModule
@@ -19,109 +13,61 @@ import retrofit2.Response
 
 class RegisterViewModel : ViewModel() {
 
+    private val _emailHasError = MutableLiveData<Boolean>()
+    val emailHasErrorLiveData: LiveData<Boolean> = _emailHasError
 
+    private val _passwordHasError = MutableLiveData<Boolean>()
+    val passwordHasErrorLiveData: LiveData<Boolean> = _passwordHasError
 
+    private val _passwordRepeatHasError = MutableLiveData<Boolean>()
+    val passwordRepeatHasErrorLiveData: LiveData<Boolean> = _passwordRepeatHasError
 
+    private val _buttonIsEnabled = MutableLiveData<Boolean>()
+    val buttonIsEnabledLiveData: LiveData<Boolean> = _buttonIsEnabled
 
-    fun initListeners(context: Context,fragment: Fragment , binding: FragmentRegisterBinding) {
-        var isEmailValid = false
-        var isPasswordValid = false
-        var isRepeatPasswordValid = false
+    private val _isRegistered = MutableLiveData<Boolean>()
+    val isRegistered: LiveData<Boolean> = _isRegistered
 
-        //  Email TextWatcher
-        binding.emailtext.editText?.addTextChangedListener {
-            if (binding.emailtext.editText?.text.toString().isNotEmpty() && validEmail(binding)) {
-                isEmailValid = true
-                binding.emailerror.text = null
-            } else {
-                isEmailValid = false
-                binding.emailerror.text = fragment.getString(R.string.invalid_email)
-            }
-            checkRegisterButtonState(binding, isEmailValid, isPasswordValid, isRepeatPasswordValid)
-        }
+    private var email: String = ""
+    private var password: String = ""
+    private var passwordRepeat: String = ""
 
-        //  Password TextWatcher
-        binding.passwordtext.editText?.addTextChangedListener {
-            if (validPassword(binding)) {
-                isPasswordValid = true
-                binding.passworderror.text = null
-            } else {
-                isPasswordValid = false
-                binding.passworderror.text = fragment.getString(R.string.short_password)
-            }
-            checkRegisterButtonState(binding, isEmailValid, isPasswordValid, isRepeatPasswordValid)
-        }
-
-        //  Repeat Password TextWatcher
-        binding.repeatPasswordText.editText?.addTextChangedListener {
-            if (validRepeatPassword(binding)) {
-                isRepeatPasswordValid = true
-                binding.passworderror.text = null
-            } else {
-                isRepeatPasswordValid = false
-                binding.passworderror.text = fragment.getString(R.string.passwords_do_not_match)
-            }
-            checkRegisterButtonState(binding, isEmailValid, isPasswordValid, isRepeatPasswordValid)
-        }
-
-        binding.registerButton.setOnClickListener {
-            sendDataToApi(
-                context,
-                fragment,
-                binding.emailtext.editText?.text.toString(),
-                binding.passwordtext.editText?.text.toString(),
-                binding.repeatPasswordText.editText?.text.toString()
-            )
-        }
+    fun emailChanged(email: String) {
+        this.email = email
+        _emailHasError.value = !Patterns.EMAIL_ADDRESS.matcher(email).matches()
+        checkRegisterButtonState()
     }
 
-    private fun validEmail(binding: FragmentRegisterBinding): Boolean {
-        val emailText = binding.emailtext.editText?.text.toString()
-
-        if (!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()) {
-            return false
-        }
-        return true
+    fun passwordChanged(password: String) {
+        this.password = password
+        _passwordHasError.value = password.length < 6
+        checkRegisterButtonState()
     }
 
-    private fun validPassword(binding: FragmentRegisterBinding): Boolean {
-        val passText = binding.passwordtext.editText?.text.toString()
-
-        if (passText.length < 6) {
-            return false
-        }
-        return true
+    fun passwordRepeatChanged(repeatPassword: String) {
+        this.passwordRepeat = repeatPassword
+        _passwordRepeatHasError.value = repeatPassword != this.password
+        checkRegisterButtonState()
     }
 
-    private fun validRepeatPassword(binding: FragmentRegisterBinding): Boolean =
-        binding.passwordtext.editText?.text.toString() == binding.repeatPasswordText.editText?.text.toString()
-
-    private fun checkRegisterButtonState(binding: FragmentRegisterBinding, email: Boolean, password: Boolean, repeatPassword: Boolean) {
-        binding.registerButton.isEnabled = email && password && repeatPassword
-        if (email && password && repeatPassword) {
-            binding.registerButton.setTextColor(Color.parseColor("#52369C"))
-        }
+    private fun checkRegisterButtonState() {
+        _buttonIsEnabled.value = _emailHasError.value == false && _passwordHasError.value == false && _passwordRepeatHasError.value == false
     }
 
-    fun sendDataToApi(context: Context, fragment: Fragment, emailData: String, passwordData: String, repeatPasswordData: String) {
+    fun register() {
         val registerRequest = RegisterRequest(
-            email = emailData,
-            password = passwordData,
-            passwordConfirmation = repeatPasswordData
+            email = this.email,
+            password = this.password,
+            passwordConfirmation = this.passwordRepeat
         )
         ApiModule.retrofit.register(registerRequest)
             .enqueue(object : Callback<RegisterResponse> {
                 override fun onResponse(call: Call<RegisterResponse>, response: Response<RegisterResponse>) {
-                    if(response.isSuccessful){
-                        val direction = RegisterFragmentDirections.toLoginFragment(true)
-                        findNavController(fragment).navigate(direction)
-                    } else {
-                        Toast.makeText(context, R.string.registration_failed, Toast.LENGTH_SHORT).show()
-                    }
+                    _isRegistered.value = response.isSuccessful
                 }
 
                 override fun onFailure(call: Call<RegisterResponse>, t: Throwable) {
-                    Toast.makeText(context, R.string.registration_failed, Toast.LENGTH_SHORT).show()
+                    _isRegistered.value = false
                 }
             })
     }
