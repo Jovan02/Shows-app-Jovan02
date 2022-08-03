e8435c7647eacac00e9aea1c4c37efc780860391
Jovan02 <46245320+Jovan02@users.noreply.github.com>
2022-08-03 10:59:35 +0200

    Create ShowDetailsViewModelFactory.kt

---
+++ app/src/main/java/com/jovannikolic/myapplication/ShowDetailsViewModelFactory.kt
@@ -0,0 +1,18 @@
+package com.jovannikolic.myapplication
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import database.ShowsDatabase
+
+class ShowDetailsViewModelFactory(
+    val database: ShowsDatabase
+) : ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+        if(modelClass.isAssignableFrom(ShowDetailsViewModel::class.java)) {
+            return ShowDetailsViewModel(database) as T
+        }
+        throw IllegalArgumentException("Sorry, we can't work with non ShowDetailsViewModel classes")
+    }
+}
