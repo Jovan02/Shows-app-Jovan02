19540c55f5b43e1a75d0590de4b7eaa05c6217c1
Jovan02 <46245320+Jovan02@users.noreply.github.com>
2022-08-03 11:00:54 +0200

    Create ReviewEntity.kt and set it as a table instead of Review.kt

--- app/src/main/java/models/Review.kt
+++ app/src/main/java/models/Review.kt
@@ -1,16 +1,12 @@
 package models
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 import kotlinx.serialization.Serializable
 
 @Serializable
-@Entity(tableName = "review")
 data class Review(
-    @PrimaryKey var id: String,
-    @ColumnInfo(name = "comment") var comment: String,
-    @ColumnInfo(name = "rating") var rating: Int,
-    @ColumnInfo(name = "show_id") var show_id: Int,
-    @ColumnInfo(name = "user") var user: User
+    var id: String,
+    var comment: String,
+    var rating: Int,
+    var show_id: Int,
+    var user: User
 )
