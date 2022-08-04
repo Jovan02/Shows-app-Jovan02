e6cab811335250aa7d9ee5e47d3f43dfe51764dd
Jovan02 <46245320+Jovan02@users.noreply.github.com>
2022-08-04 12:12:41 +0200

    Move constants from MyInterceptor.kt to Constants.kt

--- app/src/main/java/models/MyInterceptor.kt
+++ app/src/main/java/models/MyInterceptor.kt
@@ -2,21 +2,27 @@ package models
 
 import android.content.Context
 import android.content.SharedPreferences
+import models.Constants.ACCESS_TOKEN
+import models.Constants.APP
+import models.Constants.CLIENT
+import models.Constants.EXPIRY
+import models.Constants.TOKEN_TYPE
+import models.Constants.UID
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class MyInterceptor(context: Context) : Interceptor {
 
-    private var sharedPreferences: SharedPreferences = context.getSharedPreferences("LoginData", Context.MODE_PRIVATE)
+    private var sharedPreferences: SharedPreferences = context.getSharedPreferences(APP, Context.MODE_PRIVATE)
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
             .newBuilder()
-            .addHeader("token-type", sharedPreferences.getString("token-type", "")!!)
-            .addHeader("access-token", sharedPreferences.getString("access-token", "")!!)
-            .addHeader("client", sharedPreferences.getString("client", "")!!)
-            .addHeader("uid", sharedPreferences.getString("uid", "")!!)
-            .addHeader("expiry", sharedPreferences.getString("expiry", "")!!)
+            .addHeader(TOKEN_TYPE, sharedPreferences.getString(TOKEN_TYPE, "")!!)
+            .addHeader(ACCESS_TOKEN, sharedPreferences.getString(ACCESS_TOKEN, "")!!)
+            .addHeader(CLIENT, sharedPreferences.getString(CLIENT, "")!!)
+            .addHeader(UID, sharedPreferences.getString(UID, "")!!)
+            .addHeader(EXPIRY, sharedPreferences.getString(EXPIRY, "")!!)
             .addHeader("Accept", "application/json")
             .addHeader("Content-Type", "application/json")
             .build()
